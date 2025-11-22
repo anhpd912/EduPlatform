52ceac076893279d6dde361acc4ffe47bd6dbd38
ducanhwork <pdanh.work@gmail.com>
2025-11-22 18:30:45 +0700

    add: add service

---
+++ be/edu-platform-server/src/main/java/dev/danh/services/auth/AuthService.java
@@ -0,0 +1,13 @@
+package dev.danh.services.auth;
+
+import dev.danh.entities.dtos.request.*;
+import dev.danh.entities.dtos.response.AuthenticationResponse;
+import dev.danh.entities.dtos.response.IntrospectResponse;
+
+public interface AuthService {
+    AuthenticationResponse authenticate(AuthenticationRequest request);
+    IntrospectResponse introspect(IntrospectRequest request);
+    void logout(LogoutRequest token);
+    AuthenticationResponse refreshToken(RefreshRequest request);
+    AuthenticationResponse authenticate(AuthenticationGoogleRequest request);
+}
